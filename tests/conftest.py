4846bf258747ece87c06fc5540fbb19500a30515
ksrikanth1243-rgb <ksrikanth1243@gmail.com>
2026-07-05 13:13:32 +0530

    updated the conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,18 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 import pytest
 from bson.objectid import ObjectId
+import mongomock
 from app import app, mongo
 
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
+    # Overwrite the URI to an in-memory mock structure
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/testdb"
+   
+    # Force PyMongo to use the mock client wrapper
+    mongo.cx = mongomock.MongoClient()
+    mongo.db = mongo.cx.get_database("testdb")    
 
     with app.app_context():
         mongo.db.students.delete_many({})
